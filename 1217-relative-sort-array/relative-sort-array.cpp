38b628ee2602b2a84c4fe52cf442b26e62285d64
Ashish Yadav <ashishyadav414101@gmail.com>
2025-03-04 22:16:13 +0530

    Time: 1 ms (34.76%) | Memory: 11.2 MB (20.02%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        vector<int> result;
-        for (int i = 0; i < arr2.size(); i++) {
-            for (int j = 0; j < arr1.size(); j++) {
-                if (arr1[j] == arr2[i]) {
-                    result.push_back(arr1[j]);
-                    arr1[j] = -1;
-                }
+        int n1=arr1.size(), n2=arr2.size();
+        unordered_map<int,int> a2i;
+        for(int i=0; i<n2; i++)
+            a2i[arr2[i]]=i;
+        for(int i = 0 ; i < arr1.size() ;i++)
+        {
+            if(a2i.count(arr1[i]) == 0)
+            {
+                a2i[arr1[i]] = arr1.size();
             }
         }
-        sort(arr1.begin(), arr1.end());
-        for (int i = 0; i < arr1.size(); i++) {
-            if (arr1[i] != -1) {
-                result.push_back(arr1[i]);
-            }
-        }
-        return result;
+        sort(arr1.begin(), arr1.end(), [&](int x, int y){
+            if (a2i[x]==a2i[y]) return x<y;
+            return a2i[x]<a2i[y];
+        });
+        return arr1;  
     }
 };
